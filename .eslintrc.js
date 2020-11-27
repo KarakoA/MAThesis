708a88ea4f816e9989c1dbdf1d38378360ac6f61
Anton Karakochev <anton.karakochev@moia.io>
2020-11-27 16:53:02 +0200

    add linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,13 @@ module.exports = {
   env: {
     browser: true,
     commonjs: true,
-    es2021: true
+    es2021: true,
+    "jest/globals": true
   },
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended","plugin:jest/recommended"],
 
    plugins: ["jest"],
   rules: {
-    // JEST
     "jest/no-disabled-tests": "warn",
     "jest/no-focused-tests": "error",
     "jest/no-identical-title": "error",
