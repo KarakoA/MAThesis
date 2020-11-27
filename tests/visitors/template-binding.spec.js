d99bed89813fe696bf289ff3faeba0909294b9a7
Anton Karakochev <anton.karakochev@moia.io>
2020-11-27 19:18:39 +0200

    template-bindings test

--- tests/visitors/template-binding.spec.js
+++ tests/visitors/template-binding.spec.js
@@ -15,62 +15,141 @@ async function parse(tags) {
   return await linter.lintCode(template(tags), NAME);
 }
 
-// expect(actual.bindings).toHaveLength(1)
-
-//TODO tagInfo test
-//    expect(actual.tagsInfo[0].id).toBe(actual.bindings[0].target);
 describe("Parsing single", () => {
-  test("moustache binding", async () => {
-    let actual = await parse(`<div> {{VAR}} </div>`);
-    expect(actual.bindings).toStrictEqual([
-      { source: "VAR", target: expect.any(String), isEventBinding: false },
-    ]);
-  }),
-    test("v-bind binding", async () => {
-      let actual = await parse(`<div v-bind:id="VAR"/>`);
+  describe("moustache binding should yield correct", () => {
+    let tag = `<div> {{VAR}} </div>`;
+    test("bindings", async () => {
+      let actual = await parse(tag);
       expect(actual.bindings).toStrictEqual([
         { source: "VAR", target: expect.any(String), isEventBinding: false },
       ]);
     });
 
-  test("v-if binding", async () => {
-    let actual = await parse(`<div v-if="VAR === false"/>`);
-    expect(actual.bindings).toStrictEqual([
-      { source: "VAR", target: expect.any(String), isEventBinding: false },
-    ]);
+    test("tagsInfo", async () => {
+      let actual = await parse(tag);
+      expect(actual.tagsInfo).toStrictEqual([
+        { id: actual.bindings[0].target, name: "VAR", loc: expect.anything() },
+      ]);
+    });
   });
 
-  test("v-model binding", async () => {
-    let actual = await parse(`<input v-model="VAR"/>`);
-    expect(actual.bindings).toStrictEqual([
-      { source: "VAR", target: expect.any(String), isEventBinding: false },
-      { source: expect.any(String), target: "VAR", isEventBinding: false },
-    ]);
-    expect(actual.bindings[0].source).toBe(actual.bindings[1].target);
-    expect(actual.bindings[1].source).toBe(actual.bindings[0].target);
+  describe("v-bind binding should yield correct", () => {
+    let tag = `<div v-bind:id="VAR"/>`;
+    test("bindings", async () => {
+      let actual = await parse(tag);
+      expect(actual.bindings).toStrictEqual([
+        { source: "VAR", target: expect.any(String), isEventBinding: false },
+      ]);
+    });
+
+    test("tagsInfo", async () => {
+      let actual = await parse(tag);
+      expect(actual.tagsInfo).toStrictEqual([
+        { id: actual.bindings[0].target, name: "div", loc: expect.anything() },
+      ]);
+    });
   });
 
-  test("free moustache binding", async () => {
-    let actual = await parse(`{{VAR}}`);
-    expect(actual.bindings).toStrictEqual([
-      { source: "VAR", target: expect.any(String), isEventBinding: false },
-    ]);
+  describe("v-if binding should yield correct", () => {
+    let tag = `<div v-if="VAR === false"/>`;
+    test("bindings", async () => {
+      let actual = await parse(tag);
+      expect(actual.bindings).toStrictEqual([
+        { source: "VAR", target: expect.any(String), isEventBinding: false },
+      ]);
+    });
+
+    test("tagsInfo", async () => {
+      let actual = await parse(tag);
+      expect(actual.tagsInfo).toStrictEqual([
+        {
+          id: actual.bindings[0].target,
+          name: "div",
+          loc: expect.anything(),
+        },
+      ]);
+    });
   });
 
-  test("empty moustache binding", async () => {
-    let actual = await parse(`{{}}`);
-    expect(actual.bindings).toHaveLength(0);
+  describe("v-model binding should yield correct", () => {
+    let tag = `<input v-model="VAR"/>`;
+    test("bindings", async () => {
+      let actual = await parse(tag);
+      expect(actual.bindings).toStrictEqual([
+        { source: "VAR", target: expect.any(String), isEventBinding: false },
+        { source: expect.any(String), target: "VAR", isEventBinding: false },
+      ]);
+      expect(actual.bindings[0].source).toBe(actual.bindings[1].target);
+      expect(actual.bindings[1].source).toBe(actual.bindings[0].target);
+    });
+
+    test("tagsInfo", async () => {
+      let actual = await parse(tag);
+      expect(actual.tagsInfo).toStrictEqual([
+        {
+          id: actual.bindings[0].target,
+          name: "input",
+          loc: expect.anything(),
+        },
+      ]);
+    });
   });
 
-  test("event binding", async () => {
-    let actual = await parse(`<div @click="OnVAR"></div>`);
-    expect(actual.bindings).toStrictEqual([
-      { source: expect.any(String), target: "OnVAR", isEventBinding: true },
-    ]);
+  describe("unbound moustache binding should yield correct", () => {
+    let tag = `{{VAR}}`;
+    test("bindings", async () => {
+      let actual = await parse(tag);
+      expect(actual.bindings).toStrictEqual([
+        { source: "VAR", target: expect.any(String), isEventBinding: false },
+      ]);
+    });
+
+    test("tagsInfo", async () => {
+      let actual = await parse(tag);
+      expect(actual.tagsInfo).toStrictEqual([
+        { id: actual.bindings[0].target, name: "VAR", loc: expect.anything() },
+      ]);
+    });
   });
 
-  //TODO expressions
-  //compound bindings
-  //nested tags
+  describe("empty moustache binding should yield correct", () => {
+    let tag = `{{}}`;
+    test("bindings", async () => {
+      let actual = await parse(tag);
+      expect(actual.bindings).toHaveLength(0);
+    });
+
+    test("tagsInfo", async () => {
+      let actual = await parse(tag);
+      expect(actual.tagsInfo).toHaveLength(0);
+    });
+  });
+
+  describe("event binding should yield correct", () => {
+    let tag = `<div @click="OnVAR"></div>`;
+    test("bindings", async () => {
+      let actual = await parse(tag);
+      expect(actual.bindings).toStrictEqual([
+        { source: expect.any(String), target: "OnVAR", isEventBinding: true },
+      ]);
+    });
+    test("tagsInfo", async () => {
+      let actual = await parse(tag);
+      expect(actual.tagsInfo).toStrictEqual([
+        {
+          id: actual.bindings[0].source,
+          name: "div",
+          loc: expect.anything(),
+        },
+      ]);
+    });
+  });
+
+  //TODO tag info different names, check resolutions 
+
+  //TODO expressions (v-if= "not EmptyName")
+  //compound bindings (v-if="VAR || VAR2")
+  //nested tags (<div><div>{ASDF}</div></div>) 
+  //check the tags that are being bound to 
   //TODO complex variables not yet supported //topLevel:{otherLevel:0}
 });
