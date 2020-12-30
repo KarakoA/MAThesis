21aa33f0b5d86ed3d137011e1bb90cf83275cb8b
Anton Karakochev <anton.karakochev@moia.io>
2020-12-30 14:41:17 +0200

    add vue files in res dir as well

--- scripts/generate-results.sh
+++ scripts/generate-results.sh
@@ -16,12 +16,12 @@ do
 OUT_PATH_FILE=$OUT_PATH/$FILE
 echo "Creating directory..." $OUT_PATH_FILE
 mkdir -p $OUT_PATH_FILE
-#//touch $OUT_PATH_FILE/data.json
 
-#echo $OUT_PATH_FILE
 echo "Executing..." npm run generate -- $TEST_FILES_PATH/$FILE $OUT_PATH_FILE/data.json '>' $OUT_PATH_FILE/scenarios.log
 npm run generate -- $TEST_FILES_PATH/$FILE $OUT_PATH_FILE/data.json > $OUT_PATH_FILE/scenarios.log
+
 cp src/index.html $OUT_PATH_FILE/index.html
+cp $TEST_FILES_PATH/$FILE $OUT_PATH_FILE/$FILE
 echo 
 done
 echo "Done"
