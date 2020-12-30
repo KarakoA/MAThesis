231608fb7e6945123a7ce581df2cfc9c47795c2e
Anton Karakochev <anton.karakochev@moia.io>
2020-12-30 15:13:13 +0200

    scenarios.log -> scenarios.txt

--- scripts/generate-results.sh
+++ scripts/generate-results.sh
@@ -17,8 +17,8 @@ OUT_PATH_FILE=$OUT_PATH/$FILE
 echo "Creating directory..." $OUT_PATH_FILE
 mkdir -p $OUT_PATH_FILE
 
-echo "Executing..." npm run generate -s $TEST_FILES_PATH/$FILE $OUT_PATH_FILE/data.json '>' $OUT_PATH_FILE/scenarios.log
-npm run generate -s $TEST_FILES_PATH/$FILE $OUT_PATH_FILE/data.json > $OUT_PATH_FILE/scenarios.log
+echo "Executing..." npm run generate -s $TEST_FILES_PATH/$FILE $OUT_PATH_FILE/data.json '>' $OUT_PATH_FILE/scenarios.txt
+npm run generate -s $TEST_FILES_PATH/$FILE $OUT_PATH_FILE/data.json > $OUT_PATH_FILE/scenarios.txt
 
 cp src/index.html $OUT_PATH_FILE/index.html
 cp $TEST_FILES_PATH/$FILE $OUT_PATH_FILE/$FILE
