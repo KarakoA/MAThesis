74c0cac37253490ab8cd1d7c90d8bf329aa562f3
Anton Karakochev <anton.karakochev@moia.io>
2021-01-03 18:22:42 +0200

    fix regression test

--- scripts/regression_test.sh
+++ scripts/regression_test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 OUT_PATH=${INIT_CWD}/resources/output
-./scripts/generate-results.sh $OUT_PATH
-DIFF=$(git diff -- $OUT_PATH)
-DIFF_WC=$(echo $DIFF | wc -l | awk '{print $1}')
-
+#./scripts/generate-results.sh $OUT_PATH
+DIFF_WC=$(git diff -- $OUT_PATH | wc -l | awk '{print $1}')
 
 if [[ $DIFF_WC -eq 0 ]]
 then
